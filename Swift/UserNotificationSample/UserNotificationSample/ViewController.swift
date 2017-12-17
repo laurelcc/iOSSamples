d772dcd191af877675ed25c2dc0f4bb7d8eda1c9
MacSoong <laurelcc@outlook.com>
2017-12-17 16:07:15 +0800

    x

---
+++ Swift/UserNotificationSample/UserNotificationSample/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  UserNotificationSample
+//
+//  Created by Soong on 2017/12/17.
+//  Copyright © 2017年 Soong. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func addLocalNotificationAction(_ sender: UIButton) {
+        //Step 1
+        let content = UNMutableNotificationContent()
+        content.title = "Wake Up!"
+        content.body = "It is morning time!"
+        content.categoryIdentifier = "TIME_EXPIRED"
+        
+        //Step 2
+        var minute = DateComponents();
+        minute.second = 10
+        let trigger = UNCalendarNotificationTrigger(dateMatching: minute, repeats: true)
+        
+        //Step 3
+        let request = UNNotificationRequest(identifier: "MorningAlarm", content: content, trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            if let err = error{
+                print(err.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func removeNotification(_ sender: Any) {
+        let current = UNUserNotificationCenter.current()
+        current.removePendingNotificationRequests(withIdentifiers: ["MorningAlarm"])
+        
+    }
+    
+
+}
+
