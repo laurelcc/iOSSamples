48815f679703d469bd157da8d18ff47d7d6f601d
MacSoong <laurelcc@outlook.com>
2017-12-17 12:40:48 +0800

    x

--- curated/wp-content/themes/curated/searchform.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php echo home_url(); ?>/" class="searchform" method="get">
-	<button class="search-button"><i class="tm-search"></i></button>
-	<input type="text" name="s" value="<?php _e('Search', MAHA_TEXT_DOMAIN); ?>" onfocus="if(this.value=='<?php _e('Search', MAHA_TEXT_DOMAIN); ?>')this.value='';" onblur="if(this.value=='')this.value='<?php _e('Search', MAHA_TEXT_DOMAIN); ?>';" autocomplete="off" />
-</form>
